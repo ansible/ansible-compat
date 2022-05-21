fa031be947e1e77aae750958ae6844c2a4756468
Sorin Sbarnea <ssbarnea@redhat.com>
2022-05-21 10:13:22 +0100

    Fix FileExistsError failure with symlink recreation

--- src/ansible_compat/runtime.py
+++ src/ansible_compat/runtime.py
@@ -501,10 +501,13 @@ class Runtime:
         # despite documentation stating that is_file() reports true for symlinks,
         # it appears that is_dir() reports true instead, so we rely on exists().
         target = pathlib.Path(project_dir).absolute()
-        exists = link_path.exists() or link_path.is_symlink()
-        if not exists or os.readlink(link_path) != str(target):
-            if link_path.exists():
-                link_path.unlink()
+        if not link_path.exists() or (
+            link_path.is_symlink() and os.readlink(link_path) != str(target)
+        ):
+            # must call unlink before checking exists because a broken
+            # link reports as not existing and we want to repair it
+            link_path.unlink(missing_ok=True)
+            # https://github.com/python/cpython/issues/73843
             link_path.symlink_to(str(target), target_is_directory=True)
         _logger.info(
             "Using %s symlink to current repository in order to enable Ansible to find the role using its expected full name.",
