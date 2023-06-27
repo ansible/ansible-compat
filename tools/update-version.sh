fa4d4b18277a0e0e76f9b90e05fd00ffbc407c89
wangluwei <wangluwei@uniontech.com>
2023-06-27 14:09:36 +0800

    rpm: include changelog

--- tools/update-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "$0")
-VERSION=$(./tools/get-version.sh)
-mkdir -p "${DIR}/../dist"
-sed -e "s/VERSION_PLACEHOLDER/${VERSION}/" \
-    "${DIR}/../.config/python3-ansible-compat.spec" \
-    > "${DIR}/../dist/python3-ansible-compat.spec"
