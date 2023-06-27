50c26ffa79997f1f0360cba7638903b5ad246651
wangluwei <wangluwei@uniontech.com>
2023-06-27 14:09:36 +0800

    update the changelog

--- tools/update-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "$0")
-VERSION=$(./tools/get-version.sh)
-mkdir -p "${DIR}/../dist"
-sed -e "s/VERSION_PLACEHOLDER/${VERSION}/" \
-    "${DIR}/../dist/python-ansible-compat.spec.in" \
-    > "${DIR}/../dist/python-ansible-compat.spec"
