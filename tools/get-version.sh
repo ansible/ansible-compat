f6e9f394ae8e25650c15ad0f4ea5ee842b40cb25
Sorin Sbarnea <ssbarnea@redhat.com>
2023-12-29 12:19:11 +0000

    Fix packit configuration

--- tools/get-version.sh
+++ tools/get-version.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 {
-    python3 -c "import setuptools_scm" || python3 -m pip install --user setuptools-scm
+    python3 -c "import setuptools_scm" >/dev/null || {
+    if [[ "$VIRTUAL_ENV" != "" ]]; then
+        PIPARGS=""
+    else
+        PIPARGS="--user"
+    fi
+        python3 -m pip install $PIPARGS setuptools-scm
+    }
 } 1>&2  # redirect stdout to stderr to avoid polluting the output
 python3 -m setuptools_scm | \
     sed 's/Guessed Version\([^+]\+\).*/\1/'
