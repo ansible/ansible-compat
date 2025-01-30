fa136c899f991d405c25c40c62f314002a56682e
Sorin Sbarnea <ssbarnea@redhat.com>
2025-01-30 14:56:15 +0000

    Avoid get_cache_dir errors with read only virtualenvs
    Fixes: https://github.com/ansible/ansible-lint/issues/4501

--- test/test_prerun.py
+++ test/test_prerun.py
@@ -6,6 +6,8 @@ import tempfile
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import pytest
+
 if TYPE_CHECKING:
     from _pytest.monkeypatch import MonkeyPatch
 
@@ -50,5 +52,41 @@ def test_get_cache_dir_isolation_no_venv_root(monkeypatch: MonkeyPatch) -> None:
     """
     monkeypatch.delenv("VIRTUAL_ENV", raising=False)
     monkeypatch.delenv("ANSIBLE_HOME", raising=False)
-    cache_dir = get_cache_dir(Path("/"), isolated=True)
+    with (
+        pytest.warns(
+            UserWarning,
+            match=r"Project directory /.ansible cannot be used for caching as it is not writable.",
+        ),
+        pytest.warns(
+            UserWarning,
+            match=r"Using unique temporary directory .* for caching.",
+        ),
+    ):
+        cache_dir = get_cache_dir(Path("/"), isolated=True)
+    assert cache_dir.as_posix().startswith(tempfile.gettempdir())
+
+
+def test_get_cache_dir_venv_ro_project_ro(monkeypatch: MonkeyPatch) -> None:
+    """Test behaviors of get_cache_dir with read-only virtual environment and read only project directory.
+
+    Args:
+        monkeypatch: Pytest fixture for monkeypatching
+    """
+    monkeypatch.setenv("VIRTUAL_ENV", "/")
+    monkeypatch.delenv("ANSIBLE_HOME", raising=False)
+    with (
+        pytest.warns(
+            UserWarning,
+            match=r"Using unique temporary directory .* for caching.",
+        ),
+        pytest.warns(
+            UserWarning,
+            match=r"Found VIRTUAL_ENV=/ but we cannot use it for caching as it is not writable.",
+        ),
+        pytest.warns(
+            UserWarning,
+            match=r"Project directory .* cannot be used for caching as it is not writable.",
+        ),
+    ):
+        cache_dir = get_cache_dir(Path("/etc"), isolated=True)
     assert cache_dir.as_posix().startswith(tempfile.gettempdir())
